c1db4cdce5aa546b61ae038d62d1aa79dec7a87a
MayurPoptani <mayurpoptani24@gmail.com>
2022-07-19 11:17:36 +0530

    repo internet based logic changes

--- lib/repositories/recipe_repo.dart
+++ lib/repositories/recipe_repo.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:apptware_test/models/recipe_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class RecipeRepo {
 
@@ -12,10 +11,14 @@ class RecipeRepo {
 
   Future<List<RecipeModel>> fetchRecipes() async {
     try {
-      bool hasInternetConnection = await InternetConnectionChecker().hasConnection;
-      var snapshot = await FirebaseFirestore.instance.collection(collectionName).get(GetOptions(source: hasInternetConnection ? Source.server : Source.cache));
+      // bool hasInternetConnection = await InternetConnectionChecker().hasConnection;
+      var snapshot = await FirebaseFirestore.instance.collection(collectionName).get(const GetOptions(source: Source.serverAndCache));
       return snapshot.docs.map((e) => RecipeModel.fromDocument(e)).toList();
     } on FirebaseException catch (e) {
+      log(e.toString());
+      return Future.error(e);
+    } on Exception catch (e) {
+
       log(e.toString());
       return Future.error(e);
     }
